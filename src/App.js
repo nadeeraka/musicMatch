0ea4c7ea9b658cedfbddbc720749ecb51419c06d
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-06 17:45:14 +0530

    adding fetures

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import Nav from "./components/layouts/nav";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>app</h1>
-      </div>
+      <Router>
+        <div>
+          <Nav />
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/layouts/nav.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => (
+  <div>
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-0 h1 mx-auto">Music finder</span>
+    </nav>
+  </div>
+);
+
+export default Nav;
