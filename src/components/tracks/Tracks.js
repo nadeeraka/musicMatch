3b775078e6696ec4ca5f05efa09eceba13707412
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-07 13:22:36 +0530

    api added

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,14 +1,18 @@
 import React, { Component } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { Consumer } from "../../context";
-import Key from '../../util/key';
 
 export default class Tracks extends Component {
-
-    componentDidMount()
-    {
-        
-    }
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${
+          process.env.REACT_MM_KEY
+        }`
+      )
+      .then(res => console.log(res))
+      .catch(err => console.error(err));
+  }
   render() {
     return (
       <>
