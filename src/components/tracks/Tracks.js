feb530f4f5b685e747b2a78d686e25d3397a43f7
nadeeraka <nimanthanadeeraka@outlook.com>
2019-02-08 14:24:21 +0530

    api improvement 1

--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,28 +1,15 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Consumer } from "../../context";
-import Key from "../../util/key";
 import Spinner from "../layouts/spinner";
 
 export default class Tracks extends Component {
-  componentDidMount() {
-    axios
-      .get(
-        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=hot&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${Key}`
-      )
-      .then(res => {
-        console.log(res);
-        this.setState({ tracks: res.data.message.body.track_list });
-      })
-      .catch(err => console.error(err));
-  }
   render() {
     return (
       <>
         <Consumer>
           {value => {
-            const { Tracks, heading } = value;
-            if (Tracks === undefined || Tracks.length === 0) {
+            const { tracks, heading } = value;
+            if (tracks === undefined || tracks.length === 0) {
               return <Spinner />;
             } else {
               return <h1>Tracks Loaded</h1>;
--- src/context.js
+++ src/context.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import axios from "axios";
+import Key from "./util/key";
 
 const Context = React.createContext();
 
@@ -7,6 +9,17 @@ export class Provider extends Component {
     tracks: [],
     heading: "Top 10 songs "
   };
+  componentDidMount() {
+    axios
+      .get(
+        `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=hot&page=1&page_size=10&country=us&f_has_lyrics=1&apikey=${Key}`
+      )
+      .then(res => {
+        console.log(res);
+        this.setState({ tracks: res.data.message.body.track_list });
+      })
+      .catch(err => console.error(err));
+  }
   render() {
     return (
       <Context.Provider value={this.state}>
